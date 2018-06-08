1930517717d945608b4006ddd1414b8f75b600a8
shubhamkhetan <shubhamkhetan@gmail.com>
2018-06-08 14:18:28 -0400

    Delete Compare_eQTLs_caQTLs.sh

--- Compare_eQTLs_caQTLs/Compare_eQTLs_caQTLs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cat islet_meta.results.annotated.qvalue.postfreq.bestEqtl.tbl | awk '{$1=substr($1, 1, length($1)-4); print}' | tr ' ' '\t' > islet_meta.results.annotated.qvalue.postfreq.bestEqtl.mod.tbl
-
-cat WithoutWindows_All_caQTLs.txt | cut -f1 | tail -n +2 > WithoutWindows_All_caQTLs_rsids.txt
-
-grep -f WithoutWindows_All_caQTLs_rsids.txt islet_meta.results.annotated.qvalue.postfreq.bestEqtl.mod.tbl > islet_meta.results.annotated.qvalue.postfreq.bestEqtl.mod.leadCaQTLsnpsOnly.tbl
